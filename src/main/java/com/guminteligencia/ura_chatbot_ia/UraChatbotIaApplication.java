7cfc8cd745484e9903ffe1d0542a78279c3b2e2e
vitorgume <vitorhvvieira@gmail.com>
2025-09-05 14:55:11 -0300

    Ajustando timezone da API

--- src/main/java/com/guminteligencia/ura_chatbot_ia/UraChatbotIaApplication.java
+++ src/main/java/com/guminteligencia/ura_chatbot_ia/UraChatbotIaApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class UraChatbotIaApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 		SpringApplication.run(UraChatbotIaApplication.class, args);
 	}
 
